b55f55d3ccf6fd4e8e2bd0ae0b2b72764962fa9b
IssaJeran <jeransmith22@gmail.com>
2017-05-03 12:22:49 -0500

    Registration view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,20 +1,32 @@
 @extends('layouts.master')
 @section('content')
-    <form method="POST" action="{{ action('Auth\AuthController@postRegister') }}">
-        {!! csrf_field() !!}
-        <div class="form-group">
-            Email
-            <input type="email" name="email" value="{{ old('email') }}" class="form-control">
-        </div>
-        <div class="form-group">
-            Password
-            <input type="password" name="password" id="password" class="form-control">
-        </div>
-        <div class="form-group">
-            <input type="checkbox" name="remember"> Remember Me
-        </div>
-        <div>
-            <button type="submit" class="btn btn-primary">Register</button>
-        </div>
-    </form>
+    <!-- resources/views/auth/register.blade.php -->
+
+<form method="POST" action="/auth/register">
+    {!! csrf_field() !!}
+
+    <div>
+        Name
+        <input type="text" name="name" value="{{ old('name') }}">
+    </div>
+
+    <div>
+        Email
+        <input type="email" name="email" value="{{ old('email') }}">
+    </div>
+
+    <div>
+        Password
+        <input type="password" name="password">
+    </div>
+
+    <div>
+        Confirm Password
+        <input type="password" name="password_confirmation">
+    </div>
+
+    <div>
+        <button type="submit">Register</button>
+    </div>
+</form>
 @stop
